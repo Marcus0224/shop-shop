af193257b5e3d193d2d6d5b9b22a04e477d52cb3
Marcus0224 <marcstlouis04@gmail.com>
2023-01-02 14:54:53 -0500

    shopping cart storage implemented

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { pluralize } from "../../utils/helpers";
 import { useStoreContext } from '../../utils/GlobalState';
 import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
+import { idbPromise } from "../../utils/helpers";
 import CartItem from "../CartItem";
 
 function ProductItem(item) {
@@ -27,6 +28,10 @@ function ProductItem(item) {
       dispatch({
         type: UPDATE_CART_QUANTITY,
         _id: _id,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      });
+      idbPromise('cart', 'put', {
+        ...itemInCart,
         purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
       });
     } else {
